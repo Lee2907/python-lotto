fd250b5634b19d7b373c1643591d4545cf1f1e8d
Lee2907 <kamvelihle.buka2907@gmail.com>
2021-06-14 14:17:05 +0200

    commit

--- lotto.py
+++ lotto.py
@@ -1,3 +1,23 @@
 import tkinter as tk
-from PIL import Image
+from PIL import Image, ImageTk
+
+root = tk.Tk()
+root.title("Let The Game Begin!")
+root.geometry("540x720")
+root.config(bg="yellow")
+
+img = Image.open("lotto-bg.jpeg")
+render = ImageTk.PhotoImage(img)
+
+img = tk.Label(root, image=render)
+img.image = render
+img.place(x=0,y=0)
+
+pick = tk.Label(root, text = "Pick Your Numbers: ")
+pick.grid(column=1,row=1)
+
+get_pick = tk.Entry(root, width=14)
+get_pick.grid(column=2,row=1)
+
+root.mainloop()
 
--- main.py
+++ main.py
@@ -1,34 +1,31 @@
 # Lottery Machine - Lihle Buka
 import tkinter as tk
 from tkinter import messagebox
-
+import
 # initilializing tkinter
 root = tk.Tk()
 root.title("Gateway Lotto!")
 root.geometry("640x320")
-root.config(bg="peachpuff")
+root.config(bg="yellow")
 
 number_input = tk.StringVar()
-
-def input():
-    get_id.set(root)
+max_len = 13
+def length():
+    s = number_input.set()
+    if len(s) > max_len:
+        number_input.set(s[:max_len])
 
 def verify():
-    found = False
-    for x in range(len(get_id)):
-        if get_id.get() >= "03*":
-            found == True
-            messagebox.showerror("Notice","Let's Play")
-
-def condition():
-    if get_id.get() < "03*":
+    if get_id.get() < "03":
         messagebox.showinfo("Notice","You are too young. Please try again in int(get_name.get() - 18) years.")
+    elif get_id.get() >= "03":
+        messagebox.showerror("Notice","Let's Play")
+        root.destroy()
+        import lotto
     elif get_id.get() == "":
              messagebox.showerror("Notice","Invalid ID Number. Try Again")
     else:
         root.destroy()
-
-
 
 # detailing the structure
 detail = tk.Label(root, text = "Player Details").grid(column = 2,row = 1)
@@ -45,7 +42,7 @@ get_address = tk.Entry(root).grid(column=3,row=5)
 id_no = tk.Label(root, text = "ID Number").grid(column=1,row=6)
 get_id = tk.Entry(root, textvariable = number_input).grid(column=3,row=6)
 
-play = tk.Button(root,text = "Play!").grid(column=2,row=7)
+play = tk.Button(root,text = "Play!", command=verify).grid(column=2,row=7)
 
 # starting the program
 root.mainloop()
