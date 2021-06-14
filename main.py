5c719b526d5d428bbb5132f90b73f4542caa88b2
Lee2907 <kamvelihle.buka2907@gmail.com>
2021-06-14 12:34:07 +0200

    commit

--- main.py
+++ main.py
@@ -3,36 +3,49 @@ import tkinter as tk
 from tkinter import messagebox
 
 # initilializing tkinter
-lotto = tk.Tk()
-lotto.geometry("640x320")
-lotto.config(bg="peachpuff")
+root = tk.Tk()
+root.title("Gateway Lotto!")
+root.geometry("640x320")
+root.config(bg="peachpuff")
 
 number_input = tk.StringVar()
 
 def input():
-    get_id.set(lotto)
+    get_id.set(root)
+
 def verify():
-     if get_name
-         messagebox.showinfo("Notice","Let's Play.")
+    found = False
+    for x in range(len(get_id)):
+        if get_id.get() >= "03*":
+            found == True
+            messagebox.showerror("Notice","Let's Play")
+
+def condition():
+    if get_id.get() < "03*":
+        messagebox.showinfo("Notice","You are too young. Please try again in int(get_name.get() - 18) years.")
+    elif get_id.get() == "":
+             messagebox.showerror("Notice","Invalid ID Number. Try Again")
+    else:
+        root.destroy()
 
 
 
 # detailing the structure
-detail = tk.Label(lotto, text = "Player Details").grid(column = 2,row = 1)
+detail = tk.Label(root, text = "Player Details").grid(column = 2,row = 1)
 
-name = tk.Label(lotto, text = "Name").grid(column = 1,row = 3)
-get_name = tk.Entry(lotto).grid(column=3,row=3)
+name = tk.Label(root, text = "Name").grid(column = 1,row = 3)
+get_name = tk.Entry(root).grid(column=3,row=3)
 
-mail = tk.Label(lotto, text = "E-mail Address").grid(column=1,row=4)
-get_mail = tk.Entry(lotto).grid(column=3,row=4)
+mail = tk.Label(root, text = "E-mail Address").grid(column=1,row=4)
+get_mail = tk.Entry(root).grid(column=3,row=4)
 
-address = tk.Label(lotto, text = "Address").grid(column=1,row=5)
-get_address = tk.Entry(lotto).grid(column=3,row=5)
+address = tk.Label(root, text = "Address").grid(column=1,row=5)
+get_address = tk.Entry(root).grid(column=3,row=5)
 
-id_no = tk.Label(lotto, text = "ID Number").grid(column=1,row=6)
-get_id = tk.Entry(lotto).grid(column=3,row=6)
+id_no = tk.Label(root, text = "ID Number").grid(column=1,row=6)
+get_id = tk.Entry(root, textvariable = number_input).grid(column=3,row=6)
 
-play = tk.Button(lotto,text = "Play!").grid(column=2,row=7)
+play = tk.Button(root,text = "Play!").grid(column=2,row=7)
 
 # starting the program
-lotto.mainloop()
+root.mainloop()
