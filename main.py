2ef05a3ec8cadf3d68d4d814d50116d543a0f0c2
Lee2907 <kamvelihle.buka2907@gmail.com>
2021-06-15 14:12:40 +0200

    commit

--- main.py
+++ main.py
@@ -1,53 +1,58 @@
 # Lottery Machine - Lihle Buka
-import tkinter as tk
-from tkinter import messagebox
-import rsaidnumber
 
-# initilializing tkinter
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+import datetime
+import rsaidnumber
+from PIL import Image, ImageTk
+
+# initializing tkinter
 root = tk.Tk()
 root.title("Gateway Lotto!")
-root.geometry("500x500")
+root.geometry("775x426")
 root.config(bg="yellow")
 
-number_input = tk.StringVar()
+img = Image.open("Lotto1v3.jpg")
+render = ImageTk.PhotoImage(img)
 
-def callback(input):
-    if input.isemail():
-        print(input)
-        return True
-    elif input is "":
-        return True
-    else:
-        return False
+img = tk.Label(root, image=render)
+img.image = render
+img.place(x=0,y=0)
 
-def verify():
-    if get_id.get() < "03":
-        messagebox.showerror("Notice","You are too young. Please try again in int(get_name.get() - 18) years.")
-    elif get_id.get() >= "03":
-        messagebox.showinfo("Notice","Let's Play!")
-        root.destroy()
-        import lotto
-    elif get_id.get() == "":
-             messagebox.showerror("Notice","Invalid ID Number. Try Again")
-    else:
-        root.destroy()
+id_number = rsaidnumber.parse('0207296224083')
 
-# detailing the structure
 detail = tk.Label(root, text = "Player Details").grid(column = 2,row = 1)
 
 name = tk.Label(root, text = "Name").grid(column = 1,row = 3)
 get_name = tk.Entry(root).grid(column=3,row=3)
 
-mail = tk.Label(root, text = "E-mail Address").grid(column=1,row=4)
+mail = tk.Label(root, text = "Surname").grid(column=1,row=4)
 get_mail = tk.Entry(root).grid(column=3,row=4)
 
-address = tk.Label(root, text = "Address").grid(column=1,row=5)
+address = tk.Label(root, text = "E-mail Address").grid(column=1,row=5)
 get_address = tk.Entry(root).grid(column=3,row=5)
 
 id_no = tk.Label(root, text = "ID Number").grid(column=1,row=6)
-get_id = tk.Entry(root, textvariable = number_input).grid(column=3, row=6)
+get_id = tk.Entry(root, textvariable = 13).grid(column=3, row=6)
 
-play = tk.Button(root,text = "Play!", command=verify).grid(column=2,row=7)
+def verify():
+    differ = datetime.today()
+    now_date = datetime.timedelta(365.25)
+    found = False
+try:
+    for x in range(len(get_id.get())):
+        if int(get_id.get()) >= int(id_number):
+            found=True
+            if found == True:
+                messagebox.showinfo("Notice","Let's Play.")
+                import mains
+            elif int(get_id.get()) < int(id_number):\
+                    messagebox.showwarning("Notice","You are too young to play.")
+    else:
+        root.destroy()
 
-# starting the program
+play = tk.Button(root,text = "Play!").grid(column=2,row=7)
+
 root.mainloop()
---
+++ mains.py
@@ -0,0 +1,45 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+root = Tk()
+root.title("Let The Game Begin!")
+root.geometry("1365x768")
+
+img = Image.open("lotto-bg.jpeg")
+render = ImageTk.PhotoImage(img)
+
+img = Label(root, image=render)
+img.image = render
+img.place(x=0,y=0)
+NORM_FONT = ("Verdana",14)
+pick = Label(root, text = "Lotto: ")
+pick.grid(column=1,row=1)
+
+get_pick = Entry(root, width=14)
+get_pick.grid(column=2,row=1)
+
+pick_2 = Label(root, text = "Lotto Plus:")
+pick_2.grid(column=1,row=2)
+
+get_pick2 = Entry(root, width = 14)
+get_pick2.grid(column=2,row=2)
+
+pick3 = Label(root,text="Powerball: ")
+pick3.grid(column=1,row=3)
+
+get_pick3 = Entry(root,width=14)
+get_pick3.grid(column=2,row=3)
+
+sets_make = Button(root, text = "Play!",width=10)
+sets_make.grid(column=1,row =4)
+
+def gen_num():
+
+    def popupmsg(msg):
+        label = Label(root, text=msg, font=NORM_FONT)
+        label.pack(side="top", fill="x", pady=10)
+        B1 = Button(root, text="Okay", command = lambda:root.destroy,gen_num)
+        B1.pack()
+
+root.mainloop()
+
